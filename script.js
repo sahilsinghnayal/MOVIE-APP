9c1d2fc1fb2d1ae84924144ff2383ac76e8b64f1
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-03-24 22:27:51 +0530

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_url =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=09fb54c8e59d99da60d4cb69e95144ee&page=1'
+const IMG_PATH='https://image.tmdb.org/t/p/w1280'
+const SEARCH_url =
+  'https://api.themoviedb.org/3/search/movie?api_key=09fb54c8e59d99da60d4cb69e95144ee&query="'
